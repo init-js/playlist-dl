2e272e5e36822ebeec11211e5ac695825fced1dc
Jean-Sebastien Legare <jslegare@gmail.com>
2016-03-28 03:57:54 -0700

    untabify

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((python-mode
+  (setq indent-tabs-mode nil
+	tab-width 4
+	python-indent-offset 4)))
